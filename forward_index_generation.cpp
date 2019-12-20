323e3692089238371a79175e4d019c73fe023b9b
Um-e-Hani <45230898+Um-e-Hani@users.noreply.github.com>
2019-12-20 11:00:57 +0500

    Updating forward index
    simplifying parsing for forward indexing as the generated files are simplified

--- forward_index_generation.cpp
+++ forward_index_generation.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <sstream>
 #include <map>
 
 using namespace std;
@@ -23,49 +24,36 @@ int main() {
 	int lastWordID = 0;
 
 	while (docID < 16) {
+		
 		filename = to_string(docID) + path;
 		file.open(filename);
 
 		if (file.fail()) {
 			cout << "Error: Cannot open file";
-			break;
+			return -1;
 		}
 
+		outputfile << docID << endl;
+		cout << docID << endl;
+		
 		vector<string> temp;
 		vector<vector<int>> vec2d;
-		int base, length, plain = 0;
+		int plain = 0;
 		string str, word;
-
+		
 		while (!file.eof()) {
 			getline(file, str, '\n');
 			temp.push_back(str);
 		}
-
+		
 		for (int i = 0; i < 4; i++) {
 			str = temp[i];
-			const char *start = str.c_str();
-			const char *p = start;
-			base = 0; length = 0;
-			
-			while (true) {
-				while (*start == '"' || *start == ',' || *start == ' ' || *start == '-' || *start == ':' || *start == ';' ||
-					*start == '!' || *start == '?' || *start == ')' || *start == '(' || *start == '_' || *start == '/' ||
-					*start == '.' || *start == '�' || *start == '�' || *start == '�' || *start == '�' || *start == '�'
-					|| *start == '[' || *start == ']' || *start == '�' || *start == '�') { start++; base++; }
-
-				p = start;
-				while (*p != '"' && *p != ',' && *p != ' ' && *p != '-'  && *p != ':'  && *p != ';'   && *p != '.' && *p != '!'
-					&& *p != '?'  && *p != '('  && *p != ')'  && *p != '_'  && *p != '/' && *p != '�'  && *p != '�'  &&
-					*p != '�' && *p != '�' && *p != '�' && *p != '[' && *p != ']' && *p != '�' && *p != '�' &&
-					*p != '\0') { p++; length++; }
-
-				word = str.substr(base, length);
-				transform(word.begin(), word.end(), word.begin(), ::tolower);
-
+			stringstream str1(str);
+			while (getline(str1, word, ' ')) {
 				int wordID = 0;
 				map<string, int>::iterator itr;
 				itr = lexicon.find(word);
-
+				
 				if (itr != lexicon.end())
 					wordID = itr->second;
 				else {
@@ -74,10 +62,10 @@ int main() {
 					outputlex << word << endl << wordID << endl;
 					lastWordID++;
 				}
-
+				
 				int idchecker = 0;
 				bool idfound = false;
-
+				
 				while (idchecker != vec2d.size()) {
 					if (vec2d[idchecker][0] != wordID)
 						idchecker++;
@@ -86,29 +74,21 @@ int main() {
 						break;
 					}
 				}
-
+				
 				if (idfound == false) {
 					vec2d.push_back(vector<int>());
 					vec2d[idchecker].push_back(wordID);
 				}
-
+				
 				if (i == 0 || i == 1 || i == 2) {
 					vec2d[idchecker].push_back(fancy);
 				}
 				else {
 					vec2d[idchecker].push_back(plain);
 					plain++;
-				}
-				
-				if (*p == '\0') break;
-
-				start = p + 1;
-				p = start;
-				base += (length + 1);
-				length = 0;
+				}	
 			}
 		}
-
 		outputfile << docID << endl;
 		for (int i = 0; i < vec2d.size(); i++) {
 			for (int j = 0; j < vec2d[i].size(); j++) {
@@ -117,16 +97,14 @@ int main() {
 			outputfile << "." << endl;
 		}
 		outputfile << "_" <<endl;
-
 		docID++;
 		file.close();
 	}
-
 	outputfile.close();
 	cout << "Forward index file finalized." << endl;
-	outputlex << "."; 
+	outputlex << ".";
 	outputlex.close();
 	cout << "Lexicon file finalized." << endl;
-
 	system("pause");
+	return 0;
 }
