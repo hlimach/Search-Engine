0aafea83a9f646753a8dc1a612945b78d41daee0
NoorUlAin <55416412+Noor648@users.noreply.github.com>
2019-12-22 17:02:35 +0500

    Update dataset_modifier.cpp

--- dataset_modifier.cpp
+++ dataset_modifier.cpp
@@ -9,98 +9,128 @@
 using namespace std;
 
 int main() {
+	//creating file readers and writers
 	ifstream file;
 	ofstream workbook;
 
+	//declaring variables
 	int docID = 1;
+	string path = ".csv";
+	string path2 = ".txt";
+	string filename;
+	string p = "E:\\dataset2\\";
 
+	//loop for first 15 docs
 	while (docID < 16) {
-		file.open(to_string(docID) + ".csv");
-		workbook.open(to_string(docID) + ".txt");
+		filename = to_string(docID) + path;
+		file.open(filename);
+		filename = p + to_string(docID) + path2;
+		workbook.open(filename);
 
-		if (workbook.fail() || file.fail()) {
-			(file.fail()) ? cout << "Error: Cannot open reading file " : cout << "Error: Cannot open writing file ";
+		//checking whether workbook is opened sucessfully
+		if (workbook.fail()) {
+			cout << "Error: Cannot open workbook";
+			return -1;
+		}
+
+		//checking whether file is opened sucessfully
+		if (file.fail()) {
+			cout << "Error: Cannot open file";
 			return -1;
 		}
 
 		cout << docID << endl;
+		//variables
 		vector<string> temp;
-		int ccount = 0, base = 0, length = 0;
+		int comma_count = 0, base = 0, length = 0;
 		string str, word;
 
-		getline(file, str);
-		const char *mystart = str.c_str();
-		bool instring{ false };
-		for (const char* p = mystart; *p; p++) {
+		//tokenizing document
+		getline(file, str); //extracting first line
+		const char *mystart = str.c_str();    //pointer to beginning of line
+		bool instring{ false }; //check whether comma in text
+		for (const char* p = mystart; *p; p++) {   //iterate through the line
+
+			 //toggle flag if we're btw double quotes
 			if (*p == '"') {
 				instring = !instring;
-				length++;
+				mystart++;
 			}
+
+			//if comma OUTSIDE double quotes
 			else if (*p == ',' && !instring) {
-				if (ccount == 2 || ccount == 3 || ccount == 4 || ccount == 5 || ccount == 6 || ccount == 7) {
+				//required fields of author title and publishers
+				if (comma_count == 2 || comma_count == 3 || comma_count == 4) {
 					temp.push_back(str.substr(base, length));
 				}
+				//and start parsing next one
 				mystart = p + 1;
 				base += length + 1;
 				length = 0;
-				ccount++;
+				comma_count++;
 			}
-			else if (ccount == 9) {
+
+			//if it is last content field
+			else if (comma_count == 9) {
+				//iterate pointer to end of content and push it into temp vector
 				while (*p != '\0') { p++; length++; }
 				temp.push_back(str.substr(base, length - 1));
 				break;
 			}
+
+			//to increment till where the string must be broken
 			else length++;
 		}
 
-		for (int i = 0; i < 7; i++) {
-
-			if (i == 3 || i == 4 || i == 5) {
-				workbook << temp[i] << " " << endl;
-				continue;
-			}
-
+		//for parsing sentences in temp vec (size always 4 because only 4 fields extracted from each doc)
+		for (int i = 0; i < 4; i++) {
+			//get string at ith pos of them vec and start breaking down words
 			str = temp[i];
 			const char *start = str.c_str();
 			const char *p = start;
 			base = 0; length = 0;
 
 			while (true) {
+				/*
+				start points to the starting position of text where the exceptions do not exist.
+				p iterates through the text until it finds one of the exception.
+				the index of start and the length of text is tracked using base and length variables.
+				*/
 				while (*start == '"' || *start == ',' || *start == ' ' || *start == '-' || *start == ':' || *start == ';' ||
-					*start == '!' || *start == '?' || *start == ')' || *start == '(' || *start == '_' || *start == '/' ||
-					*start == '.' || *start == '€' || *start == 'â' || *start == '™' || *start == '˜' || *start == 'œ'
-					|| *start == '[' || *start == ']' || *start == '¦' || *start == '”') {
-					start++; base++;
-				}
-				
-				p = start;
-				
+										*start == '!' || *start == '?' || *start == ')' || *start == '(' || *start == '_' || *start == '/' || 
+										*start == '.' || *start == '€' || *start == 'â' || *start == '™' || *start == '˜' || *start == 'œ'
+										|| *start == '[' || *start == ']' || *start == '¦' || *start == '”') { start++; base++; }
+						p = start;
 				while (*p != '"' && *p != ',' && *p != ' ' && *p != '-'  && *p != ':'  && *p != ';'   && *p != '.' && *p != '!'
-					&& *p != '?'  && *p != '('  && *p != ')'  && *p != '_'  && *p != '/' && *p != '€'  && *p != 'â'  &&
-					*p != '™' && *p != '˜' && *p != 'œ' && *p != '[' && *p != ']' && *p != '¦' && *p != '”' &&
-					*p != '\0') {
-					p++; length++;
-				}
-
+										&& *p != '?'  && *p != '('  && *p != ')'  && *p != '_'  && *p != '/' && *p != '€'  && *p != 'â'  &&
+										*p != '™' && *p != '˜' && *p != 'œ' && *p != '[' && *p != ']' && *p != '¦' && *p != '”' &&
+										*p != '\0') { p++; length++; }
+	
+				//once p hits an exception the string is broken into substring and passed into a temporary word variable
 				word = str.substr(base, length);
+				//transforms word into lowercase to eliminate multiple storage of same word
 				transform(word.begin(), word.end(), word.begin(), ::tolower);
 				workbook << word << " ";
 
-				if (*p == '\0')
-					break;
+				//once p reaches end of line then we break out of this loop*/
+				if (*p == '\0') break;
 
+				//else we keep breaking the words in the line.
 				start = p + 1;
 				p = start;
 				base += (length + 1);
 				length = 0;
+
 			}
+
 			workbook << endl;
-			cout << "." << endl;
+			cout << "c" << endl;
 		}
 		workbook.close();
 		docID++;
 		file.close();
-		cout << "done" << endl;
+		cout << "d" << endl;
+
 	}
-	return 0;
+	system("pause");
 }
