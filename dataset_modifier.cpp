66ec819a54a60310a37f0bf1191d3b9ac416356b
Haleema Ramzan <57444629+hlimach@users.noreply.github.com>
2020-05-01 02:22:00 +0500

    Update dataset_modifier.cpp

--- dataset_modifier.cpp
+++ dataset_modifier.cpp
@@ -9,98 +9,98 @@
 using namespace std;
 
 int main() {
-	ifstream file;
-	ofstream workbook;
+    ifstream file;
+    ofstream workbook;
 
-	int docID = 1;
+    int docID = 1;
 
-	while (docID < 50001) {
-		file.open(to_string(docID) + ".csv");
-		workbook.open(to_string(docID) + ".txt");
+    while (docID < 50001) {
+        file.open(to_string(docID) + ".csv");
+        workbook.open(to_string(docID) + ".txt");
 
-		if (workbook.fail() || file.fail()) {
-			(file.fail()) ? cout << "Error: Cannot open reading file " : cout << "Error: Cannot open writing file ";
-			return -1;
-		}
+        if (workbook.fail() || file.fail()) {
+            (file.fail()) ? cout << "Error: Cannot open reading file " : cout << "Error: Cannot open writing file ";
+            return -1;
+        }
 
-		cout << docID << endl;
-		vector<string> temp;
-		int ccount = 0, base = 0, length = 0;
-		string str, word;
+        cout << docID << endl;
+        vector<string> temp;
+        int ccount = 0, base = 0, length = 0;
+        string str, word;
 
-		getline(file, str);
-		const char *mystart = str.c_str();
-		bool instring{ false };
-		for (const char* p = mystart; *p; p++) {
-			if (*p == '"') {
-				instring = !instring;
-				length++;
-			}
-			else if (*p == ',' && !instring) {
-				if (ccount == 2 || ccount == 3 || ccount == 4 || ccount == 5 || ccount == 6 || ccount == 7) {
-					temp.push_back(str.substr(base, length));
-				}
-				mystart = p + 1;
-				base += length + 1;
-				length = 0;
-				ccount++;
-			}
-			else if (ccount == 9) {
-				while (*p != '\0') { p++; length++; }
-				temp.push_back(str.substr(base, length - 1));
-				break;
-			}
-			else length++;
-		}
+        getline(file, str);
+        const char *mystart = str.c_str();
+        bool instring{ false };
+        for (const char* p = mystart; *p; p++) {
+            if (*p == '"') {
+                instring = !instring;
+                length++;
+            }
+            else if (*p == ',' && !instring) {
+                if (ccount == 2 || ccount == 3 || ccount == 4 || ccount == 5 || ccount == 6 || ccount == 7) {
+                    temp.push_back(str.substr(base, length));
+                }
+                mystart = p + 1;
+                base += length + 1;
+                length = 0;
+                ccount++;
+            }
+            else if (ccount == 9) {
+                while (*p != '\0') { p++; length++; }
+                temp.push_back(str.substr(base, length - 1));
+                break;
+            }
+            else length++;
+        }
 
-		for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) {
 
-			if (i == 3 || i == 4 || i == 5) {
-				workbook << temp[i] << " " << endl;
-				continue;
-			}
+            if (i == 3 || i == 4 || i == 5) {
+                workbook << temp[i] << " " << endl;
+                continue;
+            }
 
-			str = temp[i];
-			const char *start = str.c_str();
-			const char *p = start;
-			base = 0; length = 0;
+            str = temp[i];
+            const char *start = str.c_str();
+            const char *p = start;
+            base = 0; length = 0;
 
-			while (true) {
-				while (*start == '"' || *start == ',' || *start == ' ' || *start == '-' || *start == ':' || *start == ';' ||
-					*start == '!' || *start == '?' || *start == ')' || *start == '(' || *start == '_' || *start == '/' ||
-					*start == '.' || *start == '€' || *start == 'â' || *start == '™' || *start == '˜' || *start == 'œ'
-					|| *start == '[' || *start == ']' || *start == '¦' || *start == '”') {
-					start++; base++;
-				}
+            while (true) {
+                while (*start == '"' || *start == ',' || *start == ' ' || *start == '-' || *start == ':' || *start == ';' ||
+                    *start == '!' || *start == '?' || *start == ')' || *start == '(' || *start == '_' || *start == '/' ||
+                    *start == '.' || *start == '€' || *start == 'â' || *start == '™' || *start == '˜' || *start == 'œ'
+                    || *start == '[' || *start == ']' || *start == '¦' || *start == '”') {
+                    start++; base++;
+                }
 
-				p = start;
+                p = start;
 
-				while (*p != '"' && *p != ',' && *p != ' ' && *p != '-'  && *p != ':'  && *p != ';'   && *p != '.' && *p != '!'
-					&& *p != '?'  && *p != '('  && *p != ')'  && *p != '_'  && *p != '/' && *p != '€'  && *p != 'â'  &&
-					*p != '™' && *p != '˜' && *p != 'œ' && *p != '[' && *p != ']' && *p != '¦' && *p != '”' &&
-					*p != '\0') {
-					p++; length++;
-				}
+                while (*p != '"' && *p != ',' && *p != ' ' && *p != '-'  && *p != ':'  && *p != ';'   && *p != '.' && *p != '!'
+                    && *p != '?'  && *p != '('  && *p != ')'  && *p != '_'  && *p != '/' && *p != '€'  && *p != 'â'  &&
+                    *p != '™' && *p != '˜' && *p != 'œ' && *p != '[' && *p != ']' && *p != '¦' && *p != '”' &&
+                    *p != '\0') {
+                    p++; length++;
+                }
 
-				word = str.substr(base, length);
-				transform(word.begin(), word.end(), word.begin(), ::tolower);
-				workbook << word << " ";
+                word = str.substr(base, length);
+                transform(word.begin(), word.end(), word.begin(), ::tolower);
+                workbook << word << " ";
 
-				if (*p == '\0')
-					break;
+                if (*p == '\0')
+                    break;
 
-				start = p + 1;
-				p = start;
-				base += (length + 1);
-				length = 0;
-			}
-			workbook << endl;
-			cout << "." << endl;
-		}
-		workbook.close();
-		docID++;
-		file.close();
-		cout << "done" << endl;
-	}
-	return 0;
+                start = p + 1;
+                p = start;
+                base += (length + 1);
+                length = 0;
+            }
+            workbook << endl;
+            cout << "." << endl;
+        }
+        workbook.close();
+        docID++;
+        file.close();
+        cout << "done" << endl;
+    }
+    return 0;
 }
